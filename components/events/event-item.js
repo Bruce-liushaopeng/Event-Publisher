53ed8e625ee6844faabc3c063351247eee9fe928
bruce <bruceliu1107@gmail.com>
2023-03-15 18:33:49 -0400

    Layout wrap around, with header

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,9 @@
 import Link from "next/link"
 import classes from "./event-item.module.css"
 import Button from "../ui/button"
+import DateIcon from "../icons/date-icon"
+import AddressIcon from "../icons/address-icon"
+import ArrowRightIcon from "../icons/arrow-right-icon"
 
 function EventItem(props) {
   const { title, image, date, location, id } = props
@@ -22,14 +25,19 @@ function EventItem(props) {
           <h2>{title}</h2>
         </div>
         <div className={classes.date}>
+          <DateIcon />
           <time>{humanReadableDate}</time>
         </div>
         <div className={classes.address}>
+          <AddressIcon />
           <address>{formattedAddress}</address>
         </div>
         <div className={classes.actions}>
           <Button link={exploreLink}> 
-            Explore Event
+            <span>Explore Event </span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
            </Button>
         </div>
       </div>
