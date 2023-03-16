cf7cc5dba145295657d10c435e78a44a69652897
bruce <bruceliu1107@gmail.com>
2023-03-16 18:22:25 -0400

    get filter query from URL and handle execption cases

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React from "react"
+import { useRouter } from "next/router"
+import { getFilteredEvents } from "../../dummy-data"
+import EventList from "../../components/events/event-list"
 
 function FilteredEventsPages() {
+  const router = useRouter()
+  const filterData = router.query.slug
+  if (!filterData) {
+    return <p className="center">Loading...</p>
+  }
+  console.log(filterData)
+  const numYear = +filterData[0]
+  const numMonth = +filterData[1]
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your value!</p>
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  })
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p> No events found for the chosen filter !</p>
+  }
+
   return (
-    <div>FilteredEventsPages</div>
+    <div>
+      <EventList items={filteredEvents} />
+    </div>
   )
 }
 
