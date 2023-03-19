0c99755c9bcfbae9e261e907691c2313b4d6b463
bruce <bruceliu1107@gmail.com>
2023-03-19 09:01:27 -0400

    server side render with file system

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,28 @@
-function HomePage() {
+import fs from 'fs/promises'; // file system module, can only used on the server side. because client cannot access file system
+import path from 'path';
+function HomePage(props) {
+  const { products } = props
+
   return (
     <ul>
-        <li>
-            Product1
-        </li>
-        <li>
-            Product2
-        </li>
-        <li>
-            Product3
-        </li>
+      {products.map((product) => (
+        <li key={product.id}> {product.title} </li>
+      ))}
     </ul>
   )
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+  const jsonData = await fs.readFile(filePath) //block the execution til it done
+  const data = JSON.parse(jsonData);
+  // prepares the props for my component, happens before component render.
+  return {
+    props: {
+      //always need to return a props key
+      products: data.products
+    },
+  }
 }
 
 export default HomePage
