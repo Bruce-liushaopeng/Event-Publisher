6618e255bff8da589729d97a39984b4471a00778
bruceliu <bruceliu1107@gmail.com>
2023-03-31 16:24:09 -0400

    eventId page with server side rendering using Path, and Props

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import EventList from "../components/events/event-list";
 import { getFeaturedEvents } from "../helpers/api-util"
 
 function HomePage(props) {
-  console.log(props.featuredEvents);
   return (
     <div>
       <EventList items={props.featuredEvents} />
