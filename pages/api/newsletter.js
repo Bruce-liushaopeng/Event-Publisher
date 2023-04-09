eb2cb52cd0fa8b284e511a5deb58096007595b0d
bruceliu <bruceliu1107@gmail.com>
2023-04-09 10:33:45 -0400

    input compoennt and comment component

---
+++ pages/api/newsletter.js
@@ -0,0 +1,12 @@
+function handler(req, res) {
+  if (req.method === "POST") {
+    const userEmail = req.body.email;
+
+    if(!userEmail || !userEmail.include('@')) {
+        res.status(422).json({message: "Invalid email address."})
+        return;
+    }
+
+    
+  }
+}
